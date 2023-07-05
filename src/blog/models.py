43c34bd5d858bf2e39c55b57fcccf8ce915dd77f
vverac <viverace@gmail.com>
2023-07-05 02:44:25 -0400

    cambios  en el modelo Post

--- src/blog/models.py
+++ src/blog/models.py
@@ -12,18 +12,18 @@ class Post(models.Model):
   title = models.CharField( verbose_name='Titulo del posteo' , max_length=100)
   content = models.TextField(verbose_name='Texto del posteo',null=True, blank= True, default='texto de prueba')
   autor=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor del posteo')
-  created_ad= models.DateTimeField(auto_now=True,verbose_name='Horario del posteo' )
-  tzone=models.charfield(choices=TZ, verbose_name='Zona horaria')
+  created_at= models.DateTimeField(auto_now=True, verbose_name='Horario del posteo' )
+  tzone=models.CharField(choices=TZ, verbose_name='Zona horaria')
 
   def __str__(self):
     # __str__ metodo para hacer una representacion de cadena
     return self.title
 
-class Commet(models.Model):
+class Comment(models.Model):
   post=models.ForeignKey('Post', on_delete=models.CASCADE)
   author=models.ForeignKey(User, on_delete=models.CASCADE)
   text = models.TextField()
-  created_ad= models.DateTimeField(auto_now=True)
+  created_at= models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return f"Comentario de {self.author.username} en {self.post.title}"
